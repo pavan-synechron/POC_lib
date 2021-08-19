b0d3233811f1bd841e2d6831b0f0f93cd5ae516c
pavan-synechron <pavan.dachepalli@synechron.com>
2021-08-19 09:56:27 +0530

    initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React,{FunctionComponent,ReactNode} from 'react';
+import PropTypes from 'prop-types';
+import './button.css';
+
+/**
+ * Primary UI component for user interaction
+ */
+
+interface Props{
+    primary: boolean
+    backgroundColor:string
+    size:|'small' |'medium'|'large'
+    label?:string
+
+}
+ const Button:FunctionComponent<Props> = ({ primary, backgroundColor, size, label, ...props }) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{backgroundColor}}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+
+
+Button.defaultProps = {
+  backgroundColor: undefined,
+  primary: false,
+  size: 'medium',
+  
+};
+
+export default Button
